f75fb3912c2a841b97dfed3efe9cd82409ce9307
Luke Bennett <LukeeeeBennettPlus@gmail.com>
2021-08-27 14:19:02 +0100

    feat: initial commit

---
+++ src/components/Carousel.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>carousel</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class Carousel extends Vue {}
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>SimpleStream</h1>
+    <p>vue.code-challenge</p>
+    <Carousel />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Carousel from "./Carousel.vue";
+
+@Component({ components: { Carousel } })
+export default class Home extends Vue {}
+</script>
