6ee07cf19d8814a3d0801ef4c91f3fe72c1b8ee7
M Iftakher Ahammed <iahammed@gmail.com>
2021-09-21 17:50:33 +0100

    From Iftakher Ahammed

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,27 +1,67 @@
 <template>
-  <div @click="myClick" :style="myStyle">carousel</div>
+  <div>
+    <div @click="myClick" :style="myStyle">carousel</div>
+    <div v-for="(movie, index) in movies" :key="index" class="panel">
+      <div v-for="(film, index) in movie" :key="index">
+          <p>Title: {{ film.title }}</p>
+          <img :src="film.poster">
+          <video width="320" height="240" controls><source :src="film.video"></video>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+<script>
+// import { Component, Vue } from "vue-property-decorator";
 import axios from "axios";
 
-@Component
-export default class Carousel extends Vue {
-  async created(): Promise<void> {
-    const response = await axios.get(
-      "http://hybridtv.ss7.tv/techtest/movies.json"
-    );
-
-    console.log("movies", response.data);
+// @Component
+export default {
+  data(){
+    return {
+      movies:''    
+    }
+  },
+  created(){
+    let url = 'http://hybridtv.ss7.tv/techtest/movies.json'
+    axios.get(url)
+    .then(response => {
+      this.movies = response.data
+    })
+  },
+  methods:{
+    myClick() {
+      console.log("myClick");
+    },
+    myStyle(){
+      return { color: "red" };
+    }
   }
 
-  myClick(): void {
-    console.log("myClick");
-  }
+  // async created(): Promise<void> {
+  //   const response = await axios.get(
+  //     "http://hybridtv.ss7.tv/techtest/movies.json"
+  //   );
+  //   this.movies 
 
-  get myStyle(): Record<string, string> {
-    return { color: "red" };
-  }
+  //   console.log("movies", response.data);
+  // }
+
+  // myClick(): void {
+  //   console.log("myClick");
+  // }
+
+  // get myStyle(): Record<string, string> {
+  //   return { color: "red" };
+  // }
 }
 </script>
+<style scoped>
+img {
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  padding: 5px;
+  width: 150px;
+}
+
+</style>
